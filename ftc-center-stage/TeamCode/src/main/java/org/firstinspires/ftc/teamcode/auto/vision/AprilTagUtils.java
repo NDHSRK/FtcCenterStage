ec1d2f9ee4e84069d619390daab81344eb392f3c
Philip Young <45668270+AusreisserSF@users.noreply.github.com>
2023-11-11 16:25:12 -0800

    Imported Brandon's method for inferring the distance and angle to a target AprilTag.

--- ftc-center-stage/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/vision/AprilTagUtils.java
+++ ftc-center-stage/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/vision/AprilTagUtils.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode.auto.vision;
 
+import org.firstinspires.ftc.ftcdevcommon.AutonomousRobotException;
 import org.firstinspires.ftc.ftcdevcommon.Pair;
 import org.firstinspires.ftc.teamcode.common.RobotConstantsCenterStage;
 
 import java.util.EnumSet;
 
 public class AprilTagUtils {
+
+    private static String TAG = AprilTagUtils.class.getSimpleName();
 
     // For validation of the AprilTags on the BLUE side backdrop.
     private static final EnumSet<RobotConstantsCenterStage.AprilTagId> blueBackdropAprilTags =
@@ -25,20 +28,53 @@ public class AprilTagUtils {
     // target AprilTag from the position of its neighbor and returns the
     // distance and angle to the target AprilTag.
 
-    //**TODO Changed the order of the parameters and the return value
-    // for compatibility with other methods.
-
     public static Pair<Double, Double> inferAprilTag(RobotConstantsCenterStage.AprilTagId pTargetId,
                                                      RobotConstantsCenterStage.AprilTagId pRecognizedId,
                                                      double pDistanceToRecognizedId, double pAngleToRecognizedId) {
         if (pTargetId == pRecognizedId) // the caller shouldn't do this
             return Pair.create(pDistanceToRecognizedId, pAngleToRecognizedId); // so return the input
 
-        //**TODO Validate that the target id and the recognized id belong to the
+        // Validate that the target id and the recognized id belong to the
         // backdrop of the same alliance.
+        if (!((blueBackdropAprilTags.contains(pTargetId) && blueBackdropAprilTags.contains(pRecognizedId)) ||
+                (redBackdropAprilTags.contains(pTargetId) && redBackdropAprilTags.contains(pRecognizedId))))
+            throw new AutonomousRobotException(TAG, "Target and recognized AprilTag ids do not belong to the same alliance");
 
-        //**TODO insert math here ...
-
-        return Pair.create(pDistanceToRecognizedId, pAngleToRecognizedId); //**TODO temp
+        return calculator(pTargetId.getNumericId(), pRecognizedId.getNumericId(),
+                pDistanceToRecognizedId, pAngleToRecognizedId);
     }
+
+    // Returns a Pair where first is the inferred distance to the target AprilTag and
+    // second is the inferred angle to the target AprilTag.
+    public static Pair<Double, Double> calculator(int targetAprilTag, int givenAprilTag, double distanceGivenApril, double angleGivenApril) {
+
+        int sign = 1;
+        int endsign = 1;
+
+        //If the given AprilTag is left of the camera, the given AprilTag angle is returned as negative.
+        //As a result, we need to compensate by making it positive so the calculations can calculate the Angles and Sides of the right triangles.
+        if (targetAprilTag > givenAprilTag) {
+            sign = -1;
+        }
+
+        //It will give it a positive angle during the calculations if the target AprilTag is left of the given AprilTag as a result of unit circle properties because sine and cosine are both
+        //positive in quadrant 1 angles. However, since the AprilTag works with negative angles, we must make it negative.
+        if (targetAprilTag < givenAprilTag) {
+            endsign = -1;
+        }
+
+        //Math shown in the notebook
+
+        angleGivenApril = Math.toRadians(angleGivenApril);
+        double c = distanceGivenApril * Math.sin(angleGivenApril);
+        double f = (Math.abs(targetAprilTag - givenAprilTag) * 6) - c;
+        double d = distanceGivenApril * Math.cos(angleGivenApril);
+
+        double newAngleApril = Math.atan(f / d);
+        newAngleApril = endsign * Math.toDegrees(newAngleApril);
+        double distanceFive = Math.sqrt(d * d + f * f);
+
+        return Pair.create(distanceFive, newAngleApril);
+    }
+
 }
